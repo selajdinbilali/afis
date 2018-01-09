d64d2dc90420888d20c11a6ad9ff9c3e263e6ce0
selajdinbilali <selajdin.bilali@gmail.com>
2018-01-09 21:09:05 +0100

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
+#    -- hostname
+printf "aragorn" >> /etc/hostname
+
 #    -- time
 ln -s /usr/share/zoneinfo/Europe/Zurich /etc/localtime
 hwclock --systohc
     
 #    -- locale (langue du sys)
 mv locale.gen /etc/locale.gen
-#printf "en_US.UTF8 UTF-8" >> /etc/locale.gen
 locale-gen
-    
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-    
+
 #    -- bepo layout for the console
-printf "KEYMAP=fr-bepo\nFONT=sun12x22" >> /etc/vconsole.conf
-    
-#    -- hostname
-printf "aragorn" >> /etc/hostname
+printf "KEYMAP=fr-bepo" >> /etc/vconsole.conf
+
     
     
 #    -- initramfs
