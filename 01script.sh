8f2261bc7ca7ef8e88ec2977ea6b3b1e4d8449fd
selajdinbilali <selajdin.bilali@gmail.com>
2018-01-09 21:17:44 +0100

    Update and rename 01script.sh to README.md

--- 01script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# first and foremost
-# loadkeys fr-bepo
-# mount -o remount,size=2G /run/archiso/cowspace
-# install git and clone the repo and go to folder
-# pacman -S git
-# git clone git@github.com:selajdinbilali/afis.git
-# cd afis
-sh ./02script.sh
---
+++ README.md
@@ -0,0 +1,7 @@
+# AFIS : first and foremost
+- loadkeys fr-bepo
+- mount -o remount,size=2G /run/archiso/cowspace
+- install git and clone the repo and go to folder
+- pacman -S git
+- git clone git@github.com:selajdinbilali/afis.git
+- cd afis
