ff331cbaddae43c0ac66e28b17555195cdcbfb7f
selajdinbilali <selajdin.bilali@gmail.com>
2018-01-09 21:06:28 +0100

    Update and rename todo.sh to 02script.sh

--- 02script.sh
+++ 02script.sh
@@ -25,9 +25,14 @@ parted --script /dev/sda \
 mkfs.ext4 /dev/sda1    
 #    -- mount
 mount /dev/sda1 /mnt
+
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+sed -s 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
+rankmirrors -n 10 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+
     
 #    -- install the system
-pacstrap -i /mnt base base-devel
+pacstrap /mnt base
     
     
 #    -- create a fstab
@@ -36,4 +41,5 @@ genfstab -U /mnt >> /mnt/etc/fstab
 cp chroot.sh /mnt/chroot.sh
 cp locale.gen /mnt/locale.gen
 #    -- change root
+echo "vous etes en chroot lancer 03script.sh"
 arch-chroot /mnt /bin/bash
