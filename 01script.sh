0ece55487aa973bb30d5d6b2f652b2e67bac666f
selajdinbilali <selajdin.bilali@gmail.com>
2018-01-09 21:00:01 +0100

    Update 01script.sh

--- 01script.sh
+++ 01script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # first and foremost
+# loadkeys fr-bepo
 # mount -o remount,size=2G /run/archiso/cowspace
 # install git and clone the repo and go to folder
 # pacman -S git
 # git clone git@github.com:selajdinbilali/afis.git
 # cd afis
-sh ./keyboardandfont.sh
-sh ./todo.sh
+sh ./02script.sh
