dc0ae6fbbce20bd4e51d5b16a34a2bd7e8177273
selajdinbilali <selajdin.bilali@gmail.com>
2018-01-09 20:58:38 +0100

    Update and rename script.sh to 01script.sh

---
+++ 01script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# first and foremost
+# mount -o remount,size=2G /run/archiso/cowspace
+# install git and clone the repo and go to folder
+# pacman -S git
+# git clone git@github.com:selajdinbilali/afis.git
+# cd afis
+sh ./keyboardandfont.sh
+sh ./todo.sh
--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# first and foremost
-sh ./keyboardandfont.sh
-# pacman -Sy git --noconfirm
-# git clone git@github.com:selajdinbilali/afis.git
-# cd afis
-# mount -o remount,size=2G /run/archiso/cowspace
-
-sh ./todo.sh
