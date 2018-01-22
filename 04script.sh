a2dc61054c147c3c93f623100a00527ccdd7d114
selajdinbilali <selajdin.bilali@gmail.com>
2018-01-22 11:01:51 +0100

    last script

---
+++ 04script.sh
@@ -0,0 +1,25 @@
+ #-- son
+sudo pacman -S alsa-utils --noconfirm
+amixer sset Master unmute
+
+#-- base de xorg
+sudo pacman -S xorg-server xorg-xinit xterm rxvt-unicode mesa --noconfirm
+
+#-- drivers video
+#$ lspci | grep VGA
+# pacman -Ss xf86 | less
+#trouver et installer les drivers nécessaire
+sudo pacman -S xf86-video-vesa --noconfirm
+# xf86-video-intel libva-intel-driver libva
+#amd(ati) : xf86-video-ati mesa-vdpau
+
+#-- xorg environnement
+sudo pacman -S xorg-twm xorg-xclock --noconfirm
+#startx
+
+#-- copy xinitrc and edit from github
+
+#-- environnement de bureau
+sudo pacman -S i3 dmenu --noconfirm
+
+#copy config of i3 in ~/.config/i3/config from github
