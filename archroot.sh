79253c6077446c3cfba3e1b1a0625f97102c206a
selajdinbilali <selajdin.bilali@gmail.com>
2020-05-14 04:41:10 +0200

    adde file to launch chroot

---
+++ archroot.sh
@@ -0,0 +1,24 @@
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+hwclock --systohc
+
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+#    -- bepo layout for the console
+printf "KEYMAP=fr-bepo" >> /etc/vconsole.conf
+printf "aragorn" >> /etc/hostname
+
+pacman -S networkmanager --noconfirm
+systemctl enable NetworkManager
+echo "type root passwd"
+passwd
+
+
+# efi version
+pacman -S grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+echo "add sudo user after reboot, and install desktop"
+echo "type umount -a and reboot"
