af8a747de412bcc71dec75069bea2190e6c44a84
selajdinbilali <selajdin.bilali@gmail.com>
2020-05-14 04:11:39 +0200

    another efi update

--- 02script.sh
+++ 02script.sh
@@ -1,51 +1,15 @@
 #!/bin/bash
 
-#    -- hostname
-printf "aragorn" >> /etc/hostname
-
-#    -- time
-ln -s /usr/share/zoneinfo/Europe/Paris /etc/localtime
-hwclock --systohc --utc
-    
-#    -- locale (langue du sys)
-mv locale.gen /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-
-#    -- bepo layout for the console
-printf "KEYMAP=fr-bepo" >> /etc/vconsole.conf
-
-    
-    
-#    -- initramfs
-mkinitcpio -p linux
-    
-    
-#    -- root passwd
-echo "type the root password"
-passwd
-    
-# wifi
-#pacman -S wpa_supplicant --noconfirm
-#pacman -S dialog --noconfirm
-
-# grub and os-prober
-#pacman -S grub os-prober --noconfirm
-#grub-install --target=i386-pc /dev/sda
-#grub-mkconfig -o /boot/grub/grub.cfg
 
 # efi version
-pacman -S grub efibootmgr dosfstools os-prober mtools dhcpcd
-mkdir /boot/EFI
-mount /dev/sda1 /boot/EFI  #Mount FAT32 EFI partition 
-grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
+pacman -S grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot
 grub-mkconfig -o /boot/grub/grub.cfg
 
 #-- wired
 INTERFACE=$(ip link | grep 2: | cut -d ":" -f 2 | cut -c 2-)
 systemctl enable dhcpcd@$INTERFACE.service
 
-
-printf "first part finished\nreboot, login as root\ninstall git, clone the repo, cd to it, launch 03script.sh"
+echo "type umount -a and reboot"
 exit
 
