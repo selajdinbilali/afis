3aff94f281f81afb117679ba3c4e5f816be565a6
selajdinbilali <selajdin.bilali@gmail.com>
2020-05-14 04:14:00 +0200

    before last

--- 01script.sh
+++ 01script.sh
@@ -46,3 +46,17 @@ systemctl enable NetworkManager
 echo "type root passwd"
 passwd
 
+
+# efi version
+pacman -S grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot
+grub-mkconfig -o /boot/grub/grub.cfg
+
+#-- wired
+INTERFACE=$(ip link | grep 2: | cut -d ":" -f 2 | cut -c 2-)
+systemctl enable dhcpcd@$INTERFACE.service
+
+echo "add sudo user after reboot, and install desktop"
+echo "type umount -a and reboot"
+exit
+
