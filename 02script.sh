4827ba0f7cfc33f5a0f602b470348fd2302a3149
selajdinbilali <selajdin.bilali@gmail.com>
2020-05-15 22:17:43 +0200

    Update 02script.sh

--- 02script.sh
+++ 02script.sh
@@ -10,3 +10,18 @@ passwd beorn
 pacman -Syu sudo --noconfirm
 usermod -aG wheel beorn
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+
+#-- base de xorg
+pacman -S xorg-server xorg-apps mesa --noconfirm
+pacman -S xf86-video-vmware --noconfirm
+
+
+
+#-- environnement de bureau
+pacman -S i3 dmenu xfce4-terminal --noconfirm
+
+
+echo "setxkbmap fr bepo\nexec i3" > /home/beorn/.xinitrc
+#-- create .xinitrc and edit from github
+mkdir -p /home/beorn/.config/i3
+cp i3/config ~/.config/i3/config
