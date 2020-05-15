328e2f661ae0f2af99b0f8c1fe6d543406307a01
selajdinbilali <selajdin.bilali@gmail.com>
2020-05-15 05:41:28 +0200

    another typo

--- 01script.sh
+++ 01script.sh
@@ -29,11 +29,11 @@ pacstrap /mnt base base-devel linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # copy the second script
-cp archchroot.sh /mnt/archchroot.sh
+cp archroot.sh /mnt/archroot.sh
 
 
 # change root
-arch-chroot /mnt ./archchroot.sh
+arch-chroot /mnt ./archroot.sh
 
 exit
 
