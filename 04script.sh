046e6abce502466433500fd0e649af04d8a597f2
selajdinbilali <selajdin.bilali@gmail.com>
2020-05-15 06:00:44 +0200

    update xorg install

--- 04script.sh
+++ 04script.sh
@@ -5,19 +5,8 @@
 #amixer sset Master unmute
 
 #-- base de xorg
-sudo pacman -S xorg-server xorg-xinit xterm mesa --noconfirm
-
-#-- drivers video
-#$ lspci | grep VGA
-# pacman -Ss xf86 | less
-#trouver et installer les drivers nécessaire
-sudo pacman -S xf86-video-vesa --noconfirm
-# xf86-video-intel libva-intel-driver libva
-#amd(ati) : xf86-video-ati mesa-vdpau
-
-#-- xorg environnement
-sudo pacman -S xorg-twm xorg-xclock --noconfirm
-#startx
+sudo pacman -S xorg-server xorg-apps mesa --noconfirm
+sudo pacman -S xf86-video-vmware--noconfirm
 
 
 
@@ -28,9 +17,8 @@ sudo pacman -S i3 dmenu xfce4-terminal --noconfirm
 # maybe create the folders
 
 
-#-- copy xinitrc and edit from github
+#-- create .xinitrc and edit from github
 printf "setxkbmap fr bepo\nexec i3" > ~/.xinitrc
 mkdir -p ~/.config/i3
 cp i3/config ~/.config/i3/config
-#mkdir ~/.config/termite
-#cp termite/config ~/.config/termite/config
+
