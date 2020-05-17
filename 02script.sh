abfaa2fec1c33a5a0e0b503012c4da990496f5db
selajdinbilali <selajdin.bilali@gmail.com>
2020-05-17 03:09:20 +0200

    Update 02script.sh

--- 02script.sh
+++ 02script.sh
@@ -10,5 +10,3 @@ passwd beorn
 pacman -Syu sudo --noconfirm
 usermod -aG wheel beorn
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
-
-pacman -S xorg-apps mesa xorg-xinit xf86-video-vmware xorg-server xterm mesa xf86-video-vesa xorg-twm xorg-xclock i3 dmenu emacs --noconfirm
