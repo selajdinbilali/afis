9eeb06439b36f3bc908ec0a279f9625018c6143b
selajdinbilali <selajdin.bilali@gmail.com>
2020-05-17 03:08:47 +0200

    Update 04script.sh

--- 04script.sh
+++ 04script.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-#-- son
-#sudo pacman -S alsa-utils --noconfirm
-#amixer sset Master unmute
-
-#-- base de xorg
-sudo pacman -S xorg-server xorg-apps mesa --noconfirm
-sudo pacman -S xf86-video-vmware--noconfirm
-
-
-
-#-- environnement de bureau
-sudo pacman -S i3 dmenu xfce4-terminal --noconfirm
-
-# download dotfiles, install mkrc
-# maybe create the folders
-
+sudo pacman -S xorg-apps mesa xorg-xinit xf86-video-vmware xorg-server xterm mesa xf86-video-vesa xorg-twm xorg-xclock i3 dmenu emacs chromium nano --noconfirm
 
 #-- create .xinitrc and edit from github
 printf "setxkbmap fr bepo\nexec i3" > ~/.xinitrc
 mkdir -p ~/.config/i3
 cp i3/config ~/.config/i3/config
+
+startx
 
